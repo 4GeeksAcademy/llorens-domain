099dff927d7096fda9f8109eec6c0698d974347f
José Antonio Llorens Padilla <131461633+JoseAnt7@users.noreply.github.com>
2024-10-04 18:59:42 +0000

    modificaciones_aplicadas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,11 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  pronoun.forEach(c =>
+    adj.forEach(d => noun.forEach(e => console.log(`${c}${d}${e}.com`)))
+  );
 };
